0800a1993554aac29eec80d8cf0463bcae5d3fb8
nulc <nulc@ya.ru>
2024-12-05 09:33:39 +0800

    Add install.sh

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pervisor
+# Pervisor
+
 Pervisor - journal of titles / Первизор - журнал заголовков
+
+### Reqirements
+
+x86_64; Ubuntu 24.04; 2 Core; 4GB RAM; 40GB SSD; 10 Mbps WAN; Open port TCP+UDP: 10001
+
+### Installation
+
+1. git clone https://github.com/nulcgit/pervisor.git
+2. cd pervisor
+3. bash install.sh
+4. System will reboot
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir temp bin data apps
+chmod 777 temp
+(echo -e "$(date -u) Pervisor installation started.") >> $PWD/data/log.txt
+echo PATH="$PATH:/home/$USER/.local/bin:$PWD/bin" | sudo tee /etc/environment
+echo PERVISOR="$PWD" | sudo tee -a /etc/environment
+source /etc/environment
+sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade -yq
+sudo DEBIAN_FRONTEND=noninteractive apt install -y git docker.io docker-compose build-essential python3-dev python3-pip python3-venv tmux cron iputils-ping net-tools unzip
+sudo usermod -aG docker $USER
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+pip install pandas -q
+
+cd $PERVISOR
+rm -rf temp
+mkdir temp
+(echo -n "$(date -u) TopTagIn system is installed." >> $PWD/data/log.txt
+sudo reboot
